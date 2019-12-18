823e7bad9c0f5d96f1c6688a978009cd03485049
John Pangalos <jpangs88@gmail.com>
2019-12-18 07:06:58 +0100

    Day 14 plus starting day 15

--- day14/question2/main.go
+++ day14/question2/main.go
@@ -28,7 +28,7 @@ type equationMap map[string]equation
 type amountMap map[string]float64
 
 func main() {
-	scanner, err := fileparse.NewScanner("day14/test3.txt")
+	scanner, err := fileparse.NewScanner("day14/input.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -58,57 +58,67 @@ func main() {
 	trillion := float64(1000000000000)
 
 	oreCount := float64(0)
-	count2 := 1000
+	count := float64(1000)
 	for oreCount < trillion {
-		count := 0
-		requirements := []amountMap{}
-		toDo := []chemical{eqMap[fuel].result}
-		for len(toDo) > 0 {
-			required := make(amountMap)
-			for _, c := range toDo {
+		if trillion-oreCount > 1000000000 {
+			count += 1000
+		} else {
+			count++
+		}
+		oreCount = logic(eqMap, count)
+	}
+	// this is one more for some reason, gotta come back to this
+	// for now making count - 1
+	fmt.Printf("%f\n", count-1)
+}
+
+func logic(eqMap equationMap, amount float64) float64 {
+	count := 0
+	requirements := []amountMap{}
+	toDo := []chemical{eqMap[fuel].result}
+	for len(toDo) > 0 {
+		required := make(amountMap)
+		for _, c := range toDo {
+			if c.name == fuel {
+				required = join(required, applyEquation(eqMap, c.name, amount))
+			} else {
 				required = join(required, applyEquation(eqMap, c.name, c.amount))
 			}
-			toDo = []chemical{}
-			for k := range required {
+		}
+		toDo = []chemical{}
+		for k := range required {
 
-				for i := count - 1; i >= 1; i-- {
-					if val, ok := requirements[i][k]; ok {
-						toRemove := applyEquation(eqMap, k, val)
-						required[k] += requirements[i][k]
-						requirements[i][k] = 0
+			for i := count - 1; i >= 1; i-- {
+				if val, ok := requirements[i][k]; ok {
+					toRemove := applyEquation(eqMap, k, val)
+					required[k] += requirements[i][k]
+					requirements[i][k] = 0
 
-						for k, v := range toRemove {
-							required[k] -= v
-						}
+					for k, v := range toRemove {
+						required[k] -= v
 					}
 				}
 			}
-			for k := range required {
-				if k == ore {
-					continue
-				}
-				toDo = append(toDo, chemical{name: k, amount: required[k]})
-			}
-			requirements = append(requirements, required)
-			count++
 		}
-
-		sum := float64(0)
-		for _, v := range requirements {
-			for k, r := range v {
-				if k == ore {
-					sum += r
-				}
+		for k := range required {
+			if k == ore {
+				continue
 			}
+			toDo = append(toDo, chemical{name: k, amount: required[k]})
 		}
-		oreCount = sum
-		val := eqMap[fuel]
-		val.result.amount = float64(count2)
-		eqMap[fuel] = val
-		fmt.Println(oreCount)
-		count2++
+		requirements = append(requirements, required)
+		count++
 	}
-	fmt.Println(oreCount)
+
+	sum := float64(0)
+	for _, v := range requirements {
+		for k, r := range v {
+			if k == ore {
+				sum += r
+			}
+		}
+	}
+	return sum
 }
 
 func newChemical(input string) (chemical, error) {
---
+++ day15/question1/main.go
@@ -0,0 +1,329 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/johnny88/aoc2019/fileparse"
+)
+
+const (
+	add           = 1
+	mult          = 2
+	saveAddr      = 3
+	display       = 4
+	jumpIfTrue    = 5
+	jumpIfFalse   = 6
+	lessThan      = 7
+	equals        = 8
+	adjustRelBase = 9
+	halt          = 99
+
+	positionMode  = 0
+	immediateMode = 1
+	relativeMode  = 2
+
+	north = 1
+	south = 2
+	west  = 3
+	east  = 4
+
+	wall  = "wall"
+	empty = "empty"
+	start = "start"
+	tank  = "tank"
+
+	foundWall = 0
+	moveDone  = 1
+	foundTank = 2
+)
+
+type code struct {
+	op, firstMode, secondMode, thirdMode int
+}
+
+type coord struct {
+	x, y int
+}
+
+type areaMap map[coord]string
+
+var relativeBase = 0
+
+func main() {
+	scanner, err := fileparse.NewScanner("day15/input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer scanner.Close()
+	vals := scanner.CommaStringParseInt()
+	runIntcode(vals)
+}
+
+func runIntcode(vals []int) {
+	vals = append(vals, make([]int, 9999999)...)
+	opLengthMap := getOpLengthMap()
+	nextOp := 0
+
+	currCoord := coord{x: 0, y: 0}
+	aMap := make(areaMap)
+	aMap[currCoord] = start
+	direction := north
+
+	count := 0
+	for {
+
+		idx := nextOp
+		c := code{op: vals[nextOp]}
+		if vals[nextOp] > 4 {
+			c = parseOpCode(vals[nextOp])
+		}
+		if c.op == halt {
+			break
+		}
+		nextOp = nextOp + opLengthMap[c.op]
+		out := performOp(direction, idx, &nextOp, vals, c)
+
+		if c.op == display {
+			nCoord := nextCoord(direction, currCoord)
+
+			switch out {
+			case foundWall:
+				aMap[nCoord] = wall
+				direction = nextDirectionClockwise(currCoord, aMap, direction)
+			case moveDone:
+				aMap[nCoord] = empty
+				direction = nextDirectionCouterClockwise(currCoord, aMap, direction)
+				currCoord = nCoord
+			case foundTank:
+				aMap[nCoord] = tank
+				break
+			}
+
+			if count%10000 == 0 {
+				aMap.toString()
+				fmt.Println(out)
+			}
+
+			count++
+		}
+
+	}
+	fmt.Println(aMap)
+}
+
+func nextDirectionClockwise(c coord, a areaMap, direction int) int {
+	var nextDirection int
+	switch direction {
+	case north:
+		nextDirection = east
+	case south:
+		nextDirection = west
+	case east:
+		nextDirection = south
+	case west:
+		nextDirection = north
+	}
+
+	// nextC := nextCoord(nextDirection, c)
+	// if _, ok := a[nextC]; ok {
+	// nextDirection = nextDirectionClockwise(nextC, a, nextDirection)
+	// }
+
+	return nextDirection
+}
+
+func nextDirectionCouterClockwise(c coord, a areaMap, direction int) int {
+	var nextDirection int
+	switch direction {
+	case north:
+		nextDirection = west
+	case south:
+		nextDirection = east
+	case east:
+		nextDirection = north
+	case west:
+		nextDirection = south
+	}
+
+	// nextC := nextCoord(nextDirection, c)
+	// if _, ok := a[nextC]; ok {
+	// nextDirection = nextDirectionClockwise(nextC, a, nextDirection)
+	// }
+
+	return nextDirection
+}
+
+func nextCoord(direction int, c coord) coord {
+	newCoord := coord{}
+	switch direction {
+	case north:
+		newCoord = coord{x: c.x, y: c.y + 1}
+	case south:
+		newCoord = coord{x: c.x, y: c.y - 1}
+	case east:
+		newCoord = coord{x: c.x + 1, y: c.y}
+	case west:
+		newCoord = coord{x: c.x - 1, y: c.y}
+	}
+	return newCoord
+}
+
+func performOp(inputVal, idx int, nextOp *int, vals []int, c code) int {
+	p1, p2, p3 := getParams(vals, c, idx)
+	out := -1
+	switch c.op {
+	case add:
+		vals[p3] = p1 + p2
+	case mult:
+		vals[p3] = p1 * p2
+	case saveAddr:
+		vals[p1] = inputVal
+	case display:
+		out = p1
+	case jumpIfTrue:
+		if p1 > 0 {
+			*nextOp = p2
+		}
+	case jumpIfFalse:
+		if p1 == 0 {
+			*nextOp = p2
+		}
+	case lessThan:
+		var val int
+		if p1 < p2 {
+			val = 1
+		}
+		vals[p3] = val
+	case equals:
+		var val int
+		if p1 == p2 {
+			val = 1
+		}
+		vals[p3] = val
+	case adjustRelBase:
+		relativeBase = relativeBase + p1
+	}
+	return out
+}
+
+func parseOpCode(op int) code {
+	opStr := strconv.Itoa(op)
+	for len(opStr) < 5 {
+		opStr = strings.Join([]string{"0", opStr}, "")
+	}
+	opArr := strings.SplitN(opStr, "", 4)
+
+	op, _ = strconv.Atoi(opArr[3])
+	firstMode, _ := strconv.Atoi(opArr[2])
+	secondMode, _ := strconv.Atoi(opArr[1])
+	thirdMode, _ := strconv.Atoi(opArr[0])
+
+	return code{
+		op:         op,
+		firstMode:  firstMode,
+		secondMode: secondMode,
+		thirdMode:  thirdMode,
+	}
+}
+
+func getOpLengthMap() map[int]int {
+	return map[int]int{
+		add:           4,
+		mult:          4,
+		saveAddr:      2,
+		display:       2,
+		jumpIfTrue:    3,
+		jumpIfFalse:   3,
+		lessThan:      4,
+		equals:        4,
+		adjustRelBase: 2,
+	}
+}
+
+func getParams(arr []int, c code, idx int) (p1, p2, p3 int) {
+	switch c.op {
+	case add, mult, lessThan, equals:
+		p1 = getParam(arr, c.firstMode, idx+1)
+		p2 = getParam(arr, c.secondMode, idx+2)
+		p3 = getParam(arr, 1, idx+3)
+		if c.thirdMode == relativeMode {
+			p3 += relativeBase
+		}
+	case saveAddr:
+		p1 = getParam(arr, 1, idx+1)
+		if c.firstMode == relativeMode {
+			p1 += relativeBase
+		}
+	case jumpIfFalse, jumpIfTrue:
+		p1 = getParam(arr, c.firstMode, idx+1)
+		p2 = getParam(arr, c.secondMode, idx+2)
+	case display, adjustRelBase:
+		p1 = getParam(arr, c.firstMode, idx+1)
+	}
+	return
+}
+
+func getParam(arr []int, mode int, idx int) int {
+	p := arr[idx]
+	if mode == positionMode {
+		return arr[p]
+	} else if mode == relativeMode {
+		return arr[relativeBase+p]
+	}
+	return p
+}
+
+func opToString(op int) string {
+	opStringMap := map[int]string{
+		add:           "add",
+		mult:          "mult",
+		saveAddr:      "saveAddr",
+		display:       "display",
+		jumpIfTrue:    "jumpIfTrue",
+		jumpIfFalse:   "jumpIfFalse",
+		lessThan:      "lessThan",
+		equals:        "equals",
+		adjustRelBase: "adjustRel",
+	}
+	return opStringMap[op]
+}
+
+func (a areaMap) toString() string {
+	var minX, minY, maxX, maxY int
+	var s string
+	for k := range a {
+		if k.x < minX {
+			minX = k.x
+		}
+		if k.y < minY {
+			minY = k.y
+		}
+		if k.x > maxX {
+			maxX = k.x
+		}
+		if k.y > maxY {
+			maxY = k.y
+		}
+	}
+	fmt.Println(minY, minX, maxY, maxX)
+	for j := minY; j > maxY; j++ {
+		for i := minX; i > maxX; i++ {
+			v := a[coord{x: i, y: j}]
+			switch v {
+			case wall:
+				s = strings.Join([]string{s, "1"}, "")
+			case empty:
+				s = strings.Join([]string{s, "2"}, "")
+			case tank:
+				s = strings.Join([]string{s, "3"}, "")
+			case start:
+				s = strings.Join([]string{s, "4"}, "")
+			}
+		}
+		s = strings.Join([]string{s, "\n"}, "")
+	}
+	return s
+}
